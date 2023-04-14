1bb4cd98682feff02242e0ed3ebba8187528f827
mio <48112523+seed330@users.noreply.github.com>
2023-04-14 13:58:20 +0800

    Update mpconfigboard.h

--- boards/MP405M/mpconfigboard.h
+++ boards/MP405M/mpconfigboard.h
@@ -27,33 +27,33 @@
 #define MICROPY_HW_RTC_USE_CALOUT   (1)
 
 // UART config
-#define MICROPY_HW_UART1_NAME   "UART1"
+
 #define MICROPY_HW_UART1_TX     (pin_A9)
 #define MICROPY_HW_UART1_RX     (pin_A10)
-#define MICROPY_HW_UART1_NAME   "UART2"
+
 #define MICROPY_HW_UART2_TX     (pin_A2)
 #define MICROPY_HW_UART2_RX     (pin_A3)
-#define MICROPY_HW_UART1_NAME   "UART6"
+
 #define MICROPY_HW_UART6_TX     (pin_A11)
 #define MICROPY_HW_UART6_RX     (pin_A12)
 
 // I2C busses
-#define MICROPY_HW_I2C1_NAME "IIC1"
+
 #define MICROPY_HW_I2C1_SCL (pin_B6)
 #define MICROPY_HW_I2C1_SDA (pin_B7)
-#define MICROPY_HW_I2C2_NAME "IIC2"
+
 #define MICROPY_HW_I2C2_SCL (pin_B10)
 #define MICROPY_HW_I2C2_SDA (pin_B11)
-#define MICROPY_HW_I2C2_NAME "IIC3"
+
 #define MICROPY_HW_I2C3_SCL (pin_A8)
 #define MICROPY_HW_I2C3_SDA (pin_B8)
 // SPI busses
-#define MICROPY_HW_SPI1_NAME "SPI1"
+
 #define MICROPY_HW_SPI1_NSS  (pin_A4) 
 #define MICROPY_HW_SPI1_SCK  (pin_A5) 
 #define MICROPY_HW_SPI1_MISO (pin_A6) 
 #define MICROPY_HW_SPI1_MOSI (pin_A7) 
-#define MICROPY_HW_SPI2_NAME "SPI2"
+
 #define MICROPY_HW_SPI2_NSS  (pin_B12) 
 #define MICROPY_HW_SPI2_SCK  (pin_B13) 
 #define MICROPY_HW_SPI2_MISO (pin_B14) 
